8676cdc62a170093d01c1dc876b1e64fffa2f803
SarunWu <96522041+SarunWu@users.noreply.github.com>
2022-03-03 16:22:43 +0800

    Format code style and fix rubocop warning

--- short_string_packer.rb
+++ short_string_packer.rb
@@ -7,9 +7,9 @@ module ShortStringPacker
   #   str - String object
   # Returns: a Integer object
   def self.pack(str)
-    str.split("")
-       .map { |m| (m.ord - 96)}
-       .reduce{ |r1, r2| r1 << 5 | r2 }
+    str.split('')
+       .map { |m| (m.ord - 96) }
+       .reduce { |r1, r2| r1 << 5 | r2 }
   end
 
   ## Unpacks a Integer from pack() method into a short string
@@ -20,32 +20,8 @@ module ShortStringPacker
     packed.to_s(2)
           .reverse
           .scan(/.{1,5}/)
-          .map{ |m| (m.reverse.to_i(2) + 96).chr }
+          .map { |m| (m.reverse.to_i(2) + 96).chr }
           .reverse
           .join
   end
 end
-
-# First Attempt
-# Pack #
-# ("1" + str.split("")
-#           .map { |m| (m.ord - 96)
-#                        .to_s(2)
-#                        .rjust(5, "0") }
-#           .reduce{ |r1,r2| r1 + r2 })
-#   .to_i(2)
-# -----------------------------------
-# Unpack #
-# packed.to_s(2)[1..-1]
-#   .scan(/.{5}/)
-#   .map{|m| (m.to_i(2) + 96).chr}
-#   .reduce { |r1, r2| r1 + r2}
-# -----------------------------------
-# packer_bench.rb #
-# pack  0.365504   0.003657   0.369161 (  0.373507)
-# unpack  0.495524   0.004674   0.500198 (  0.501806)
-# -----------------------------------
-
-# Second(current) Attempt
-# pack  0.151116   0.001523   0.152639 (  0.154351)
-# unpack  0.479838   0.003516   0.483354 (  0.483684)
